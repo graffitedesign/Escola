868f78e6b7202fc4a4494fe827001cd26ef1cbec
graffitedesign <graffitedesign@gmail.com>
2017-09-14 01:53:51 -0300

    Finalmente Apenas 1 Projeto

---
+++ public_html/index.php
@@ -0,0 +1,88 @@
+<?php
+	include './header.php';
+?>
+
+			<div class="album text-muted">
+		      <div class="container">
+
+		        <div class="row">
+		          <div class="card">
+		            <img data-src="js/holder.min.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">Prepare-se para o cargo de Analista Judiciário do TST com curso teórico focado nos principais pontos do edital.</p>
+		            
+		            <div class="btn btn-toolbar">
+		              <button class="text-left"><a href="#">Comprar</a></button>
+		              <button class="text-right"><a href="#">Comprar</a></button>
+		            </div>
+
+		          </div>
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+		          <div class="card">
+		            <img data-src="holder.js/100px280/thumb" alt="Card image cap">
+		            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		          </div>
+		        </div>
+
+		      </div>
+		    </div>
+
+		    <footer class="text-muted">
+		      <div class="container">
+		        <p class="float-right">
+		          <a href="#">Back to top</a>
+		        </p>
+		        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+		        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+		      </div>
+		    </footer>
+
+		    <!-- Bootstrap core JavaScript
+		    ================================================== -->
+		    <!-- Placed at the end of the document so the pages load faster -->
+		    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+
+		    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+
+		    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		    <script src="../../assets/js/vendor/holder.min.js"></script>
+		    <script>
+		      $(function () {
+		        Holder.addTheme("thumb", { background: "#55595c", foreground: "#eceeef", text: "Thumbnail" });
+		      });
+		    </script>
+		    <script src="../../dist/js/bootstrap.min.js"></script>
+		    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+		</div><!--conteudo -->
+<?php
+	include './rodape.php';
+?>
